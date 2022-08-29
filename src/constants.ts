07c8bcd9c91b57ec1e8709f0b40b3af40871f112
Jesús Acuña <34004828+ManuelMaciel@users.noreply.github.com>
2022-08-29 17:49:25 -0400

    add images and background

---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+import vulnerableStatus from "data-base64:~assets/warning-sign.png"
+import secureStatus from "data-base64:~assets/insurance.png"
+import backgroundSvg from "data-base64:~assets/svg.svg"
+
+export {
+    vulnerableStatus,
+    secureStatus,
+    backgroundSvg
+}
